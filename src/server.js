82575bbe40d399fd11260ca5818d76303412d9ad
septa97 <jancharles.adona@gmail.com>
2017-08-01 10:33:20 +0800

    feat: Added body-parser module

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import debug from 'debug';
+import bodyParser from 'body-parser';
 import { green } from 'chalk';
 
 const app = express();
 const log = debug('server');
 const PORT = process.env.PORT || 8000;
+
+// Load middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Load the routes
 require('./routes').default(app);
