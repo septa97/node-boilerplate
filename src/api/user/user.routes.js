e8cd1ea40396d5eac5025ace3154986c3a63641a
septa97 <jancharles.adona@gmail.com>
2017-08-26 21:37:28 +0800

    feat(user): Add a validation middleware

--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import validate from 'express-validation';
 import * as controller from './user.controller';
+import validation from './user.validation';
 
 const router = express.Router();
 
-router.get('/', controller.getAll);
-router.get('/:id', controller.getUser);
+router.get('/users', controller.getAll);
+router.get('/user/:id', validate(validation.getUser), controller.getUser);
 
 export default router;
