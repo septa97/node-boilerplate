fd377325ff4555c818da9d24945ce001eba5c82d
septa97 <jancharles.adona@gmail.com>
2017-08-26 21:38:00 +0800

    test(user): Modularized test by module and then by route

--- src/api/user/user.tests.js
+++ src/api/user/user.tests.js
@@ -6,20 +6,24 @@ import app from './../../server';
 const should = chai.should();
 const request = supertest(app);
 
-describe('User', () => {
-  it('should get all the users', done => {
-    request.get('/api/user').expect(200).end((err, res) => {
-      should.not.exist(err);
-      should.exist(res);
-      done();
+describe('User API', () => {
+  describe('GET /api/users', () => {
+    it('should get all the users', done => {
+      request.get('/api/users').expect(200).end((err, res) => {
+        should.not.exist(err);
+        should.exist(res);
+        done();
+      });
     });
   });
 
-  it('should get a user by id', done => {
-    request.get('/api/user/1').expect(200).end((err, res) => {
-      should.not.exist(err);
-      should.exist(res);
-      done();
+  describe('GET /api/user/:id', () => {
+    it('should get a user by id', done => {
+      request.get('/api/user/1').expect(200).end((err, res) => {
+        should.not.exist(err);
+        should.exist(res);
+        done();
+      });
     });
   });
 });
