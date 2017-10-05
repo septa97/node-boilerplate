43d5a5ae08e54a63ef1a9935aa07400d03159769
septa97 <jancharles.adona@gmail.com>
2017-10-05 13:13:04 +0800

    fix(user.model): Add role attribute

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -7,6 +7,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
